fb317b4b8e53462917bfd54b838d9b93d20400bc
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-05-27 18:52:22 +0800

    cmdtest

--- src/main/java/com/example/main/AnalyzerApplication.java
+++ src/main/java/com/example/main/AnalyzerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnalyzerApplication {
 
 	public static void main(String[] args) {
+		System.out.print("main start\n");
 		Manager manager = new Manager();
 		manager.testResult();
 		SpringApplication.run(AnalyzerApplication.class, args);
