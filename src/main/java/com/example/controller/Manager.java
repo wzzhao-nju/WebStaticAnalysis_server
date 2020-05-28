7dffb3963ba17008894a926a3f50106bb31581e0
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-05-28 23:23:14 +0800

    writer

--- src/main/java/com/example/controller/Manager.java
+++ src/main/java/com/example/controller/Manager.java
@@ -20,13 +20,13 @@ public class Manager {
     public void getResult(String filename) {
         try {
             Runtime runtime = Runtime.getRuntime();
-            runtime.exec("clang++ -emit-ast -c " + filename);
+            Process temp = runtime.exec("clang++ -emit-ast -c " + filename);
+            System.out.print(temp.waitFor());
             FileWriter writer = new FileWriter("astList.txt", false);
             writer.write(filename.substring(0, filename.lastIndexOf('.')) + ".ast");
             writer.close();
-            Process p = runtime.exec("../SE-Experiment-master/cmake-build-debug/tools/Checker/Checker astList.txt config.txt");
-            int returnValue = p.waitFor();
-            System.out.print(returnValue + "\n");
+            Process p = runtime.exec(new String[]{"../SE-Experiment-master/cmake-build-debug/tools/Checker/Checker", "astList.txt", "config.txt"});
+            System.out.print(p.waitFor());
         }catch (IOException | InterruptedException e){
             e.printStackTrace();
         }
