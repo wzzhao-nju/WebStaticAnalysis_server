0cee6d91ddd2a5693e18760618dd8edbc85bb7a9
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-05-28 22:04:24 +0800

    ComponentScan

--- src/main/java/com/example/main/AnalyzerApplication.java
+++ src/main/java/com/example/main/AnalyzerApplication.java
@@ -4,8 +4,10 @@ import com.example.controller.Manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = "com.example.*")
 public class AnalyzerApplication {
 
 	public static void main(String[] args) {
