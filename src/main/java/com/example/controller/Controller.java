0428c0d204cd37ff2bb478c0d35ec93cbc2882aa
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-06-02 15:48:32 +0800

    chineseHint

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,7 +17,11 @@ import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -35,16 +38,21 @@ public class Controller {
     public Message Post(@RequestBody CodeLine codeline){
         //对字符串进行转存
         String[] code = codeline.getCodeline().split("\n");
-        String filename = RandomStringUtils.randomAlphabetic(8) + ".cpp"; //由于没有文件名，这里随机生成一个 todo 注册系统上线后，改成 用户名+时间
+        String identity = UUID.randomUUID().toString().replaceAll("-", "") + ".cpp"; //由于没有文件名，这里随机生成一个UUID todo 注册系统上线后，改成 用户名+时间
+        Vector<String> filenames = new Vector<>();
         try {
-            FileWriter writer = new FileWriter(savepath + filename);
+            File dst = new File(savepath + identity + "/" + identity + ".cpp");
+            if(!dst.exists()) dst.mkdirs();
+            FileWriter writer = new FileWriter(dst);
             for (String s : code)
                 writer.write(s + "\n");
         } catch (IOException e){
             e.printStackTrace();
-            return new Message(-1, filename, "Server could not restore your code! Ask admin for details.");
+            return new Message(-1, null, "服务器无法缓存您的代码，请联系管理员。");
         }
-        return new Message(0, filename);
+        filenames.add(identity);
+        identity_filename.put(identity, filenames);
+        return new Message(0, identity);
     }
 
     //传输文件 todo 压缩包
@@ -56,17 +64,17 @@ public class Controller {
         //文件名不能为空，避免程序崩溃
         if(filename == null){
             log.info("Logger: filename is null!");
-            return new Message(-1, null, "Filename is null!");
+            return new Message(-1, null, "文件名不能为空！");
         }
         //文件是否为空
         if(upload.isEmpty()){
             log.info(String.format("Logger: file %s is empty", filename));
-            return new Message(-1, null, "File is empty!");
+            return new Message(-1, null, "文件不能为空！");
         }
         //格式检查
         if(!filename.endsWith(".c") && !filename.endsWith(".cpp") && !filename.endsWith(".h") && !filename.endsWith(".zip")){
             log.info(String.format("Logger: the format of file %s is wrong!", filename));
-            return new Message(-1, null, "File format is wrong!");
+            return new Message(-1, null, "文件格式错误！");
         }
         //转存
         try{
@@ -98,7 +106,7 @@ public class Controller {
         } catch (IOException | ZipException e){
             log.info(String.format("Logger: restore file %s failed", filename));
             e.printStackTrace();
-            return new Message(-1, null, "Server could not restore your file! Ask admin for details.");
+            return new Message(-1, null, "服务器无法缓存您的文件，请联系管理员。");
         }
         log.info(String.format("Logger: download %s finished!\n", filename));
         return new Message(0, identity);
@@ -106,6 +114,10 @@ public class Controller {
 
     @PostMapping("/api/getResult")
     public void testResult(@RequestBody Identity identity) {
+        String id = identity.getIdentity();
+        if(identity_filename.containsKey(id)){
+            
+        }
         //new Manager().getResult(filename.getFilename());
     }
 
