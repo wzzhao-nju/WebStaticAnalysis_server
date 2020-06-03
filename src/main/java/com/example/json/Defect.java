c69f99d498a74b31fef2e48a5f6a557108f745c2
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-06-03 13:03:48 +0800

    sortFixed

--- src/main/java/com/example/json/Defect.java
+++ src/main/java/com/example/json/Defect.java
@@ -21,6 +21,13 @@ public class Defect implements Comparable<Defect>{
     }
 
     public int compareTo(Defect defect){
-        return this.location.compareTo(defect.location);
+        String afilename = this.location.substring(0, this.location.indexOf(':'));
+        String bfilename = defect.location.substring(0, defect.location.indexOf(':'));
+        int aNo = Integer.parseInt(this.location.substring(this.location.indexOf(':') + 1, this.location.lastIndexOf(':')));
+        int bNo = Integer.parseInt(defect.location.substring(defect.location.indexOf(':') + 1, defect.location.lastIndexOf(':')));
+        if(afilename.compareTo(bfilename) != 0)
+            return afilename.compareTo(bfilename);
+        else
+            return Integer.compare(aNo, bNo);
     }
 }
