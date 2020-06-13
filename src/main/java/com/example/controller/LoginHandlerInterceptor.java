5a4911856e592593e642972a6c1b06f891f14afa
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-06-13 18:32:40 +0800

    loginoutOK

--- src/main/java/com/example/controller/LoginHandlerInterceptor.java
+++ src/main/java/com/example/controller/LoginHandlerInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Component
@@ -23,11 +24,27 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         Object uid = request.getSession().getAttribute("uid");
-        if(uid == null)
+        response.setCharacterEncoding("UTF-8");
+        try {
+            if(uid == null) {
+                response.setStatus(430);
+                response.getWriter().append("请先登录");
+                return false;
+            }
+            if((Integer)uid == -1)
+                return true;
+            List<LoginInfo> loginInfoList = loginInfoRepository.findByUid((Integer) uid);
+            if(loginInfoList.size() <= 0 || loginInfoList.get(0).getSessionId().equals(request.getSession().getId()))
+                return true;
+            else{
+                response.setStatus(430);
+                response.getWriter().append("登录已过期, 或者已在其他地方登录");
+                return false;
+            }
+        }catch (IOException e) {
+            //这里不应该被执行
+            e.printStackTrace();
             return false;
-        if((Integer)uid == -1)
-            return true;
-        List<LoginInfo> loginInfoList = loginInfoRepository.findByUid((Integer) uid);
-        return loginInfoList.size() <= 0 || loginInfoList.get(0).getSessionId().equals(request.getSession().getId());
+        }
     }
 }
