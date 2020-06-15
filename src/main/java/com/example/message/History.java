900b75fce590fbf19ee24213d3cb58fcc517b3f8
Koishi <55008838+stnanzhao@users.noreply.github.com>
2020-06-15 11:03:59 +0800

    historyFixed

--- src/main/java/com/example/message/History.java
+++ src/main/java/com/example/message/History.java
@@ -9,10 +9,44 @@ public class History {
     private Integer filecount;
     private Integer errorcount;
 
+    public History(){}
+
     public History(String analyzeId, Timestamp timestamp, Integer filecount, Integer errorcount){
         this.analyzeId = analyzeId;
         this.timestamp = timestamp;
         this.filecount = filecount;
         this.errorcount = errorcount;
+    }
+
+    public void setAnalyzeId(String analyzeId) {
+        this.analyzeId = analyzeId;
+    }
+
+    public String getAnalyzeId() {
+        return analyzeId;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setFilecount(Integer filecount) {
+        this.filecount = filecount;
+    }
+
+    public Integer getFilecount() {
+        return filecount;
+    }
+
+    public void setErrorcount(Integer errorcount) {
+        this.errorcount = errorcount;
+    }
+
+    public Integer getErrorcount() {
+        return errorcount;
     }
 }
